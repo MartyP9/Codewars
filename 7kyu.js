9058e00c3cc30d8d7e2d62d2a630baaa68e53646
Peter Martens <superiorbrews@gmail.com>
2023-04-10 15:32:31 -0500

    Added Largest Elements Kata

--- 7kyu.js
+++ 7kyu.js
@@ -67,3 +67,11 @@ function isItANum(s) {
 }
 
 //  Other solution const isItANum = str =>(str.replace(/\D/g,'').match(/^0\d{10}$/) || ['Not a phone number'])[0];
+
+
+//Largest Elements
+// Write a program that outputs the top n elements from a list.
+// Example:
+// largest(2, [7,6,5,4,3,2,1])
+// // => [6,7]
+const largest = (n,x) => n ? x.sort((a,b)=>a-b).slice(-n): [];
