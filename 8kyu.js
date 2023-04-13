7a86bad226f3ed8b434437048854b4bd36296b99
Peter Martens <superiorbrews@gmail.com>
2023-04-13 17:58:44 -0500

    Added Square(n) Sum Kata

--- 8kyu.js
+++ 8kyu.js
@@ -72,6 +72,10 @@ function feast(beast, dish) {
   return (beast[0] === dish[0] && beast[beast.length -1] === dish[dish.length -1])
   }
 
-  //Sum of positive
-  
-  const positiveSum = (arr) => arr.filter(x => x > 0).reduce((a,b) => a+b, 0)
+//Sum of positive
+
+const positiveSum = (arr) => arr.filter(x => x > 0).reduce((a,b) => a+b, 0)
+
+// Square(n) Sum
+
+const squareSum = (numbers) => numbers.reduce((a,b) => a + b**2 , 0)
