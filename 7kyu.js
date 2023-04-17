ba58f09388695f28c45ec72969cd7e499a814a1f
Peter Martens <superiorbrews@gmail.com>
2023-04-17 10:43:36 -0500

    Added Odd or Even? Kata

--- 7kyu.js
+++ 7kyu.js
@@ -118,3 +118,10 @@ const binaryArrayToNumber = n => parseInt(n.join(''), 2);
 function rowSumOddNumbers(n) {
 	return n**3;
 }
+
+//  Odd or Even?
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+
+const oddOrEven = a => a.reduce((a,b) => a + b,0) % 2 === 0 ? 'even':'odd';
