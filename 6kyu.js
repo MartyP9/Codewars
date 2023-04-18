78478b3dd496542beb008a0e19b4c7934bc7d3ea
Peter Martens <superiorbrews@gmail.com>
2023-04-18 15:18:13 -0500

    Added stop spinning my words! Kata

--- 6kyu.js
+++ 6kyu.js
@@ -14,3 +14,16 @@ function solution (n){
     }
     return sum;
 }
+
+//  Stop gninnipS My sdroW!
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+function spinWords(words){
+    return words.split(' ').map(function (word) {
+      return (word.length > 4) ? word.split('').reverse().join('') : word;
+    }).join(' ');
+  }
+// shorter solution
+  function spinWords(str){
+    return str.split(' ').map( w => w.length<5 ? w : w.split('').reverse().join('') ).join(' ');
+  }
