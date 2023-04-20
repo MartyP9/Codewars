0e5c6b756c44f7f373fc08917b649c2d35703a02
Peter Martens <superiorbrews@gmail.com>
2023-04-20 14:07:31 -0500

    Added Highest and Lowest Kata

--- 7kyu.js
+++ 7kyu.js
@@ -135,4 +135,20 @@ const oddOrEven = a => a.reduce((a,b) => a + b,0) % 2 === 0 ? 'even':'odd';
 
 const squareDigits = n => +(n.toString().split('').map(a => (+a) ** 2).join(""))
 
-console.log(digit(215))
+//  Highest and Lowest
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// Examples
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes: All numbers are valid Int32, no need to validate them. There will always be at least one number in the input string. Output string must be two numbers separated by a single space, and highest number is first.
+function highAndLow (n){
+    let s = n.toString().split(' ').sort((a,b) => +b - +a)
+    return `${s[0]} ${s[s.length-1]}`
+}
+
+//Other solution
+// function highAndLow(numbers){
+//   numbers = numbers.split(' ');
+//   return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+// }
