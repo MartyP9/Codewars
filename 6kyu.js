b05cc4ee746bc25c931db51b23c9a71548ab61da
Peter Martens <superiorbrews@gmail.com>
2023-04-20 10:30:48 -0500

    Added Sum of Digits/Digital Root Kata

--- 6kyu.js
+++ 6kyu.js
@@ -27,3 +27,18 @@ function spinWords(words){
   function spinWords(str){
     return str.split(' ').map( w => w.length<5 ? w : w.split('').reverse().join('') ).join(' ');
   }
+
+//   Sum of Digits / Digital Root
+
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+  function digitalRoot(n) {
+    if (n < 10) return n;
+    
+    return digitalRoot(
+      n.toString().split('').reduce((a,b)=> a + +b, 0));
+  }
+
+// Math Trick solution. Casting 9's
+// const digitalRoot = n => (n-1) % 9 + 1;
