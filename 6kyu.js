eb24bae68aac1d4ca5a6b25a71be153d98520e40
Peter Martens <superiorbrews@gmail.com>
2023-04-24 09:56:18 -0500

    Added Array.diff Kata

--- 6kyu.js
+++ 6kyu.js
@@ -42,3 +42,13 @@ function spinWords(words){
 
 // Math Trick solution. Casting 9's
 // const digitalRoot = n => (n-1) % 9 + 1;
+
+//   Array.diff
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order. If a value is present in b, all of its occurrences must be removed from the other.
+
+const arrayDiff = (a,b) => a.filter(x=> b.indexOf(x) === -1)
+
+// Other solution 
+//const array_diff = (a, b) => a.filter(x => !b.includes(x));
