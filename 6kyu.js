c247f543ca8556b3e68f2706083c2b89b31db585
Peter Martens <superiorbrews@gmail.com>
2023-04-27 10:27:36 -0500

    Added Find Unique number kata

--- 6kyu.js
+++ 6kyu.js
@@ -92,3 +92,10 @@ function tribonacci (arr,n){
   }
   return arr.slice(0,n)
 }
+
+//   Find the unique number
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// It’s guaranteed that array contains at least 3 numbers.
+// The tests contain some very huge arrays, so think about performance.
+
+const findUniq = arr => +arr.filter((a) => arr.indexOf(a) === arr.lastIndexOf(a))
