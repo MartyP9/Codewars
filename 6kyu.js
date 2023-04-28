0fda5081c1179bbdf3c2d983cf135175b966f956
Peter Martens <superiorbrews@gmail.com>
2023-04-28 13:55:32 -0500

    Added remove the minimum Kata

--- 6kyu.js
+++ 6kyu.js
@@ -106,3 +106,4 @@ function sortArray(arr) {
   const odd = arr.filter((x) => x % 2).sort((a,b) => a - b);
   return arr.map((x) => x % 2 ? odd.shift() : x);
 }
+
--- 7kyu.js
+++ 7kyu.js
@@ -200,3 +200,22 @@ const isSquare = n => n % Math.sqrt(n) === 0 || n == 0
 // function isSquare(n) {
 //     return Math.sqrt(n) % 1 === 0;} 
 // const isSquare = n => Number.isInteger(Math.sqrt(n)); 
+
+
+//     Remove the minimum
+//The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+function removeSmallest(numbers) {
+    const min = Math.min.apply(this, numbers);
+    return numbers.filter((num, idx, arr) => idx !== arr.indexOf(min));
+  }
+// Other solutions
+// const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
