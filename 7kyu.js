950d83bfa811886448450b8eaf4cf2ed087bf012
Peter Martens <superiorbrews@gmail.com>
2023-05-01 10:11:18 -0500

    Added Testing 1-2-3 Kata

--- 7kyu.js
+++ 7kyu.js
@@ -219,3 +219,10 @@ function removeSmallest(numbers) {
   }
 // Other solutions
 // const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
+
+//     Testing 1-2-3
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+const number = (arr)=> arr.map((x,i)=> (`${i+1}: ${x}`))
