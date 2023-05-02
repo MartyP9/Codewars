20d9ecfd77ed760878ac68e7bce0d3a4ef63c212
Peter Martens <superiorbrews@gmail.com>
2023-05-02 13:46:59 -0500

    Added Don't give me five! Kata

--- 7kyu.js
+++ 7kyu.js
@@ -226,3 +226,15 @@ function removeSmallest(numbers) {
 // The numbering starts at 1. The format is n: string. Notice the colon and space in between.
 
 const number = (arr)=> arr.map((x,i)=> (`${i+1}: ${x}`))
+
+//    Don't give me five!
+//In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive! The result may contain fives. ;-)
+//The start number will always be smaller than the end number. Both numbers can be also negative!
+function dontGiveMeFive(start, end) {
+    let count = 0;
+    for (let i = start; i <= end; ++i)
+        if (!i.toString().includes("5")){
+            count++;
+        }
+    return count;
+}
