877f23c27edb3df2c387e98263bc1c3ea6bacdb1
Peter Martens <superiorbrews@gmail.com>
2023-05-05 11:29:15 -0500

    Added Grasshopper - If/else syntax debug kata

--- 8kyu.js
+++ 8kyu.js
@@ -135,3 +135,11 @@ function well(arr){
 // You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
 
 const index = (a,n) => isNaN(a[n]**n) ?-1: a[n]**n
+
+//     Grasshopper - If/else syntax debug
+
+// While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+// checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+// The function receives one parameter health which will always be a whole number between -10 and 10.
+
+const checkAlive = n => n>0 
