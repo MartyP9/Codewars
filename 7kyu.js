545f952ad7cac7973dddd46c7c23f8a766305e77
Peter Martens <superiorbrews@gmail.com>
2023-05-18 10:05:39 -0500

    Added Jaden Casing Strings kata

--- 7kyu.js
+++ 7kyu.js
@@ -272,3 +272,11 @@ const XO = (str) => {
   // Other solution   function XO(str) {
   //   return str.toLowerCase().split('x').length === str.toLowerCase().split('o').length;
   // }
+
+//    Jaden Casing Strings
+//When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map((word) => word[0].toUpperCase() + word.substring(1)).join(" ");
+  };
