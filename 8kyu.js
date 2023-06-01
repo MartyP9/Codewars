f1a9bcce360b96abff5f59e66e4a1cb0e6cd27b5
Peter Martens <superiorbrews@gmail.com>
2023-06-01 15:11:34 -0500

    Added Remove first and Last Character Kata

--- 8kyu.js
+++ 8kyu.js
@@ -183,3 +183,7 @@ const numToString = (num) => num.toString()
 // Other solutions
 // const numberToString = num => `${num}`
 // const numberToString = num => String(num)
+
+//   Remove First and Last Character
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+const removeChar = str => str.slice(1,-1)
