5bf4d6fd9f583c322b3a628ed5a2a84cc5fd0ac1
Peter Martens <superiorbrews@gmail.com>
2023-06-07 20:18:17 -0500

    updated the info on last commit

--- 8kyu.js
+++ 8kyu.js
@@ -195,6 +195,7 @@ const stringToNumber = str => Number(str);
 //Other possible solution 
 // const stringToNumber = str => +str;
 
+//     Are You Playing Banjo?
 // Create a function which answers the question "Are you playing banjo?".
 // If your name starts with the letter "R" or lower case "r", you are playing banjo!
 // The function takes a name as its only argument, and returns one of the following strings:
