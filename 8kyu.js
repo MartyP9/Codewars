544b96e826eeeb3427804f18715e5f90e0d50d08
Peter Martens <superiorbrews@gmail.com>
2023-06-18 07:56:06 -0500

    Added boolean kata

--- 8kyu.js
+++ 8kyu.js
@@ -229,3 +229,6 @@ const formatMoney = amount=> "$" + amount.toFixed(2)
 function makeNegative(num) {
   return -Math.abs(num);
 }
+//   Convert boolean values to strings 'Yes' or 'No'.
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+const boolToWord = bool => bool?'Yes':'No'
