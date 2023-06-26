8ab727fe20eabc64d8587abbeaa5078e5e1a0405
Peter Martens <superiorbrews@gmail.com>
2023-06-25 23:07:31 -0500

    added Century from year kata

--- 8kyu.js
+++ 8kyu.js
@@ -236,3 +236,9 @@ const boolToWord = bool => bool?'Yes':'No'
 //      Opposite Number
 // Very simple, given an integer or a floating-point number, find its opposite.
 const opposite = number => -number
+
+//     Century From Year
+//The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Given a year, return the century it is in.
+
+const century = year => Math.ceil(year/100)
