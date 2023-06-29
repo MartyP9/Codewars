87ba05b9da5d7e3809e10cefbabb0ff1245112c7
Peter Martens <superiorbrews@gmail.com>
2023-06-29 08:13:04 -0500

    Added Keep Hydrated Kata

--- 8kyu.js
+++ 8kyu.js
@@ -242,3 +242,10 @@ const opposite = number => -number
 // Given a year, return the century it is in.
 
 const century = year => Math.ceil(year/100)
+
+//    Keep Hydrated!
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+const water = (time) => Math.floor(time*.5)
