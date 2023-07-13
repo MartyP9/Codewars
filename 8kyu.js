c297ddefe18af9123312746a690ca9dba4cca698
Peter Martens <superiorbrews@gmail.com>
2023-07-13 11:37:15 -0500

    Added fake binary Kata

--- 8kyu.js
+++ 8kyu.js
@@ -263,3 +263,21 @@ const abbrevName = name => name.split(' ').map(i => i[0].toUpperCase()).join('.'
 // Implement a function which convert the given boolean value into its string representation.
 // Note: Only valid inputs will be given.
 const booleanToString = b => b.toString()
+
+
+//     Fake Binary
+
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(x){
+  let result ='' 
+  for (let i of x){
+   (i < 5)? result += '0' : result +='1';
+ }
+ return result;
+}
+
+// Other Solution 
+// function fakeBin(x) {
+//   return x.split('').map(n => n < 5 ? 0 : 1).join('');
+// }
