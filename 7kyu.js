90bf83e613d28525d5952322ea46520e330785c7
Peter Martens <superiorbrews@gmail.com>
2023-09-08 10:32:24 -0500

    Added most digits kata

--- 7kyu.js
+++ 7kyu.js
@@ -492,3 +492,7 @@ const explode = s => {
     return sorted.concat(sorted[0]).join('')
 }
 //  const solution = (a, b) => a.length > b.length ? b+a+b : a+b+a;
+
+//    Most digits
+// Find the number with the most digits. If two numbers in the argument array have the same number of digits, return the first one in the array.
+const findLongest = arr => arr.reduce((a, b) => (`${b}`.length > `${a}`.length) ? b : a);
