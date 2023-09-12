6b4bac6119bef2f73e7ace10d13a2bfb63220717
Peter Martens <superiorbrews@gmail.com>
2023-09-12 14:09:14 -0500

    Added who will pay for wall? kata

--- 8kyu.js
+++ 8kyu.js
@@ -367,3 +367,9 @@ const nameShuffler =str=> str.split(' ').reverse().join(' ')
 
 // As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
 const reverse= string=> string.split(' ').reverse().join(' ')
+
+//     Who is going to pay for the wall?
+// Your code will show Full name of the neighbor and the truncated version of the name as an array. If the number of the characters in name is less than or equal to two, it will return an array containing only the name as is"
+function whoIsPaying(name){
+  return name.length>2?[name, name.slice(0,2)]:[name]
+}
