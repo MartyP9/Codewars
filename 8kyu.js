455feb5c5b0380a2c9927ef0d61b789fd1ebfae0
Peter Martens <superiorbrews@gmail.com>
2023-09-14 18:12:58 -0500

    Added EUFA Euro 2016 kata

--- 8kyu.js
+++ 8kyu.js
@@ -386,3 +386,13 @@ const billboard = (name, price = 30) => +(name.length / (1 / price))
 //   } 
 //   return totalCost;
 //   }
+
+//   UEFA Euro 2016
+// Finish the uefaEuro2016() function so it return string just like in the examples below:
+// uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+// uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+// uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+function uefaEuro2016(teams, scores){
+  let result = scores[1]>scores[0]?`${teams[1]} won!`: scores[1]<scores[0] ? `${teams[0]} won!`:`teams played draw.`
+  return `At match ${teams[0]} - ${teams[1]}, ${result}`
+}
