57c3b72e84e95ca4cfcc6456073d0164d0892fdd
Peter Martens <superiorbrews@gmail.com>
2023-09-21 10:19:24 -0500

    Added Terminal Game#1 kata

--- 8kyu.js
+++ 8kyu.js
@@ -425,3 +425,14 @@ function convertToCelsius (temperature) {
   let celsius = (temperature - 32) * (5/9)
   return celsius
 }
+
+//    Grasshopper - Terminal Game # 1
+// In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes: name = user argument or 'Hero', position = '00', health = 100, damage = 5, experience = 0
+
+function Hero (name='Hero',position,health,damage, experience) {
+  this.name = name;
+  this.position = '00';
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
