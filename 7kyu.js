747822190bc5ef43c6a67dd6dd13081f58e078e2
Peter Martens <superiorbrews@gmail.com>
2023-10-21 11:36:12 -0500

    Added Friend or Foe kata

--- 7kyu.js
+++ 7kyu.js
@@ -530,3 +530,8 @@ const countDevelopers = (list) => {
 }
 // Other Solution
 // const countDevelopers =(list) => list.filter(x=>x.continent=='Europe'&&x.language=='JavaScript').length
+
+// Friend or Foe?
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+const friend = (friends) => friends.filter(x=>x.length === 4)
