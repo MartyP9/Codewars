942c749f359e7036cb12edb07c23eaf5f7f6f720
Peter Martens <superiorbrews@gmail.com>
2023-10-23 10:03:37 -0500

    Added Categorize new member kata

--- 7kyu.js
+++ 7kyu.js
@@ -546,3 +546,9 @@ function basicOp(o, a, b) {
   return eval(a+o+b);
 }
 // Can also use a switch case on the operation value
+
+//  Categorize new Member
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+const openOrSenior = (data)=> data.map(e=> e[0]>=55 && e[1]>7?'Senior':'Open')
