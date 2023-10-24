d8f02abcd9ca3d519e05ec9f3dffe5f23002bf07
Peter Martens <superiorbrews@gmail.com>
2023-10-24 10:28:36 -0500

    Added Is n div by x and y Kata

--- 8kyu.js
+++ 8kyu.js
@@ -458,3 +458,7 @@ class SmallestIntegerFinder {
 //     return Math.min(...args)
 //   }
 // }
+
+// Is n divisible by x and y?
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+const isDivisible = (n,x,y)=> n%x===0 && n%y===0
