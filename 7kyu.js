47a69e33d7c8919e8ad5cdec8cf5506de31558f0
Peter Martens <superiorbrews@gmail.com>
2023-11-05 14:21:13 -0600

    Added sort by last char Kata

--- 7kyu.js
+++ 7kyu.js
@@ -620,3 +620,11 @@ const elevatorDistance = arr => {
 // Other solution with reduce
 // const elevatorDistance = arr => arr.reduce((acc,x,i)=>acc += Math.abs(x-arr[i-1] ||0),0)
 
+//    Sort by Last Char     7
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.All inputs will be valid.
+
+const last = s => s.split(' ').sort((a,b)=> a.charCodeAt(a.length -1) - b.charCodeAt(b.length -1))
+
+// Other Solution
+// const last = ( $ ) => $.split(' ').sort((a,b)=> a.slice(-1) > b.slice(-1))
