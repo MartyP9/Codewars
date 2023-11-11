681b701bc82af53bccb3072172ef2806cd59a8e1
Peter Martens <superiorbrews@gmail.com>
2023-11-11 12:06:26 -0600

    Added

--- 8kyu.js
+++ 8kyu.js
@@ -477,3 +477,8 @@ const mergeArrays = (a, b) => [...new Set(a.concat(b))].sort((a,b)=>a-b);
 // If the number has an integer square root, take this, otherwise square the number.
 
 const squareOrSquareRoot = array => array.map(n => Math.sqrt(n)%1===0 ? Math.sqrt(n): n*n)
+
+
+//     Is he gonna survive?
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+const hero = (bullets, dragons) => bullets>= dragons*2
