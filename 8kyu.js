62060ae03858a2ad03216f26acff094211569c3b
Peter Martens <superiorbrews@gmail.com>
2023-11-22 14:23:12 -0600

    Added String Templates - Bug Fixes #5

--- 8kyu.js
+++ 8kyu.js
@@ -547,3 +547,10 @@ const isDigit = s => s == parseFloat(s)
 var sortArray = function(value) {
   return value.split('').sort((c, p) => c - p ).join('');
 }
+
+//    String Templates - Bug Fixes #5
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+function buildString(...template){
+  return `I like ${template.join(', ')}!`;
+}
