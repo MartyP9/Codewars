9563c58301fa9a339484f1f6bf7ed9dab99afb39
Peter Martens <superiorbrews@gmail.com>
2024-01-05 09:04:57 -0600

    Added Invalid Login Bug Fixing11 kata

--- 8kyu.js
+++ 8kyu.js
@@ -664,3 +664,10 @@ class Ship {
 //   Double char
 // Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 const doubleChar = str => str.split('').map(s=>s.repeat(2)).join('')
+
+//    Invalid Login - Bug Fixing #11
+// Your task is simple, search the password string for any injected code (Injected code is any thing that would be used to exploit flaws in the current code, so basically anything that contains || or //) if you find any you must return "Wrong username or password!" because no one likes someone trying to cheat their way in!   
+function validate(username, password){
+  var database = new Database();
+  return database.login(username, encodeURI(password));
+}
