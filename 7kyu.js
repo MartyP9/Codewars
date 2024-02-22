b49ad039bea17b4d0561c4c59f6d807ed74165ad
Peter Martens <superiorbrews@gmail.com>
2024-02-22 17:48:41 -0600

    Added Consecutive items kata

--- 7kyu.js
+++ 7kyu.js
@@ -724,3 +724,11 @@ const averages = arr =>{
 function towerCombination(n) {
   return n === 1 ? 1n : BigInt(n) * BigInt(towerCombination(n-1))
 }
+
+//   Consecutive items
+// You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise). It is guaranteed that a and b are both present in arr.
+
+const consecutive = (arr, a, b) => arr.indexOf(a) + 1 === arr.indexOf(b) ? true : arr.indexOf(b) + 1 === arr.indexOf(a)? true: false
+
+// Other solution 
+//cont consecutive = (arr, a, b) => Math.abs(arr.indexOf(a) - arr.indexOf(b)) == 1;
